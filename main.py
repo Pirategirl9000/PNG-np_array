74ad96ec7a75ba9c86b8b4bb426f79d7d7eef07d
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-04-08 15:34:51 +0000

    complete

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from skimage import io
+import sys
+
+im = io.imread("image.png") #RGBA format
+imarr = np.array(im)
+
+#remove alpha
+imarr = imarr[ :, :, 0:3]
+
+f = open("file.txt", "w")
+
+np.set_printoptions(threshold=sys.maxsize) #stop truncation
+
+f.write(f"{imarr}")
+f.close()
